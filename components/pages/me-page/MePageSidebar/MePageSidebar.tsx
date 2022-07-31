ec55fdff866c0d9684f69452d6e01d67ab142d51
Martynov <kseexy@mail.ru>
2022-07-31 14:27:56 +0300

    feat: make me page

--- components/pages/me-page/MePageSidebar/MePageSidebar.tsx
+++ components/pages/me-page/MePageSidebar/MePageSidebar.tsx
@@ -3,6 +3,6 @@ import { PageSidebar } from '~/components/shared/PageSidebar';
 import { Header } from './components/Header';
 import { Content } from './components/Content';
 
-export const IndexPageSidebar: FC = (): ReactElement => {
+export const MePageSidebar: FC = (): ReactElement => {
     return <PageSidebar header={<Header />} content={<Content />} />;
 };
--- pages/me/index.tsx
+++ pages/me/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { IndexPageSidebar } from '~/components/pages/index-page/IndexPageSidebar';
+import { MePageSidebar } from '~/components/pages/me-page/MePageSidebar';
 import Layout from '~/components/shared/Layout';
 
 const Index = () => {
-    return <Layout sidebar={<IndexPageSidebar />} content={<div>Content</div>} />;
+    return <Layout sidebar={<MePageSidebar />} content={<div>Content</div>} />;
 };
 
 export default Index;
