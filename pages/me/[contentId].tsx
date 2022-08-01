dbbe486a5054b6ab4a6522f36e1b444ee6343e88
Martynov <kseexy@mail.ru>
2022-08-01 09:00:55 +0300

    add static paths

--- pages/me/[contentId].tsx
+++ pages/me/[contentId].tsx
@@ -1,8 +1,38 @@
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
 import React from 'react';
-import { MePage } from '~/components/pages/me-page/MePage';
+import { MePage, ME_CONTENT_IDS } from '~/components/pages/me-page/MePage';
 
 const Page = () => {
     return <MePage />;
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [
+            {
+                params: {
+                    contentId: ME_CONTENT_IDS.ADD,
+                },
+            },
+            {
+                params: {
+                    contentId: ME_CONTENT_IDS.ALL,
+                },
+            },
+            {
+                params: {
+                    contentId: ME_CONTENT_IDS.PENDING,
+                },
+            },
+        ],
+        fallback: false,
+    };
+};
+
+export const getStaticProps: GetStaticProps = (context: GetStaticPropsContext) => {
+    return {
+        props: {},
+    };
 };
 
 export default Page;
